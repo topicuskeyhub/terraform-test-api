6d9c264f99f5a660cb5334394f7361705f4a3a67
Emond Papegaaij <emond.papegaaij@topicus.nl>
2024-01-11 18:46:41 +0100

    Put terraformrc in correct place

--- docker/build.sh
+++ docker/build.sh
@@ -15,19 +15,5 @@ cd terraform-provider-keyhub
 go generate ./...
 go install .
 
-cat << EOF > ~/.terraformrc
-provider_installation {
-
-  dev_overrides {
-      "registry.terraform.io/topicuskeyhub/keyhub" = "/go/bin"
-  }
-
-  # For all other providers, install them directly from their origin provider
-  # registries as normal. If you omit this, Terraform will _only_ use
-  # the dev_overrides block, and so no other providers will be available.
-  direct {}
-}
-EOF
-
 cd ../terraform-test-api
 go install .
