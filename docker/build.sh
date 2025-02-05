1d0ea168690c0a18c1468029ede746524cc73fcd
Emond Papegaaij <emond.papegaaij@topicus.nl>
2025-02-05 09:45:18 +0100

    Upgrade terraform

--- docker/build.sh
+++ docker/build.sh
@@ -4,8 +4,8 @@ set -xe
 
 apt update
 apt -y install unzip libicu72
-wget https://releases.hashicorp.com/terraform/1.9.5/terraform_1.9.5_linux_amd64.zip
-unzip -d /go/bin terraform_1.9.5_linux_amd64.zip
+wget https://releases.hashicorp.com/terraform/1.10.5/terraform_1.10.5_linux_amd64.zip
+unzip -d /go/bin terraform_1.10.5_linux_amd64.zip
 
 cd /work
 go work init
